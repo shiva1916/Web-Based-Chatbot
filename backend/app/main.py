009c05cc721d02f02838215386c92b51f8db5ecd
shiva1916 <126543573+shiva1916@users.noreply.github.com>
2025-06-06 13:55:03 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,20 @@
-print("Backend coming soon")
+from fastapi import FastAPI, UploadFile, File
+import pytesseract
+from PIL import Image
+import io
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Wasserstoff!"}
+
+@app.post("/ocr")
+async def extract_text(file: UploadFile = File(...)):
+    try:
+        image_bytes = await file.read()
+        image = Image.open(io.BytesIO(image_bytes))
+        text = pytesseract.image_to_string(image)
+        return {"extracted_text": text}
+    except Exception as e:
+        return {"error": str(e)}
